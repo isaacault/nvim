5eae2d3997d9a14ae64c03450f09b77f022ad72e
Isaac Ault <isaac.ault@gmail.com>
2024-09-16 09:36:40 +0100

    Omit notifications for plugin updates.

--- lua/isaacault/lazy.lua
+++ lua/isaacault/lazy.lua
@@ -30,5 +30,8 @@ require("lazy").setup({
     -- colorscheme that will be used when installing plugins.
     --install = { colorscheme = { "catppuccin" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
 })
