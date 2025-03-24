502c2f97aced65f950636b6773b39ad0e10f49d4
Isaac Ault <isaac.ault@codeplay.com>
2025-03-24 13:09:33 +1000

    Modifications.

--- lua/isaacault/plugins/lsp.lua
+++ lua/isaacault/plugins/lsp.lua
@@ -219,18 +219,21 @@ return {
 			cmake = {}, -- CMake
 
 			lua_ls = { -- Lua
-				-- cmd = { ... },
-				-- filetypes = { ... },
-				-- capabilities = {},
-				-- settings = {
-				--   Lua = {
-				--     completion = {
-				--       callSnippet = 'Replace',
-				--     },
-				--     -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-				--     -- diagnostics = { disable = { 'missing-fields' } },
-				--   },
-				-- },
+				settings = {
+					Lua = {
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								"vim",
+								"require",
+							},
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					},
+				},
 			},
 		}
 
@@ -271,101 +274,3 @@ return {
 		})
 	end,
 }
-
--- return {
---   "neovim/nvim-lspconfig",
---   dependencies = {
---     "williamboman/mason.nvim",
---     "williamboman/mason-lspconfig.nvim",
---     "hrsh7th/cmp-nvim-lsp",
---     "hrsh7th/cmp-buffer",
---     "hrsh7th/cmp-path",
---     "hrsh7th/cmp-cmdline",
---     "hrsh7th/nvim-cmp",
---     "L3MON4D3/LuaSnip",
---     "saadparwaiz1/cmp_luasnip",
---     "j-hui/fidget.nvim",
---   },
---   event = { "BufReadPost", "BufNewFile" },
---   cmd = { "LspInfo", "LspInstall", "LspUninstall" },
---   config = function()
---     -- Set up lspconfig.
---     local capabilities = require('cmp_nvim_lsp').default_capabilities()
---     require("fidget").setup()
---     require("mason").setup()
---     require("mason-lspconfig").setup({
---       PATH = "append",
---       ensure_installed = {
---         "lua_ls",
---         "rust_analyzer",
---         "clangd",
---       },
---       handlers = {
---         function(server_name)
---           require("lspconfig")[server_name].setup {
---             capabilities = capabilities
---           }
---         end,
---         ["lua_ls"] = function()
---           local lspconfig = require("lspconfig")
---           lspconfig.lua_ls.setup {
---             capabilities = capabilities,
---             settings = {
---               Lua = {
---                 diagnostics = {
---                   globals = { "vim" }
---                 }
---               }
---             }
---           }
---         end,
---       },
---     })
---
---     -- -- Don't ensure_installed clangd with to allow for local clangd
---     -- require("lspconfig")['clangd'].setup {
---     --   cmd = {
---     --     "clangd",
---     --     "--header-insertion=never",
---     --   },
---     -- }
---
---     -- Set up nvim-cmp.
---     local cmp = require('cmp')
---     local cmp_select = { behavior = cmp.SelectBehavior.Select }
---
---     cmp.setup({
---       snippet = {
---         -- REQUIRED - you must specify a snippet engine
---         expand = function(args)
---           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
---         end,
---       },
---       mapping = cmp.mapping.preset.insert({
---         ['<C-e>'] = cmp.mapping.abort(),
---         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---         ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---         ['<C-Space>'] = cmp.mapping.complete(),
---       }),
---       sources = cmp.config.sources({
---         { name = 'nvim_lsp' },
---         { name = 'luasnip' }, -- For luasnip users.
---       }, {
---         { name = 'buffer' },
---       })
---     })
---
---     vim.diagnostic.config({
---       -- update_in_insert = true,
---       float = {
---         focusable = false,
---         style = "minimal",
---         border = "rounded",
---         source = "always",
---         header = "",
---         prefix = "",
---       },
---     })
---   end,
--- }
--- lua/isaacault/plugins/nvim-treesitter.lua
+++ lua/isaacault/plugins/nvim-treesitter.lua
@@ -1,35 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = {
-          "c",
-          "cmake",
-          "cpp",
-          "lua",
-          "python",
-          "rust",
-          "vim",
-          "vimdoc",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            -- Use <Enter> to begin and increment selection of the tree.
-            init_selection = "<Enter>",
-            node_incremental = "<Enter>",
-            scope_incremental = false,
-            node_decremental = "<Backspace>",
-          },
-        },
-      })
-    end
-  },
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"cmake",
+					"cpp",
+					"lua",
+					"python",
+					"vim",
+					"vimdoc",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						-- Use <Enter> to begin and increment selection of the tree.
+						init_selection = "<Enter>",
+						node_incremental = "<Enter>",
+						scope_incremental = false,
+						node_decremental = "<Backspace>",
+					},
+				},
+			})
+		end,
+	},
 }
